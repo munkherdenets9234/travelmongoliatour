3d69c87210bb62c2e14a6614e16f9a03616e2e63
rda new <munkherdene.ts9234@gmail.com>
2026-07-03 20:56:26 +0800

    loading

---
+++ src/app/[locale]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="h-[3px] w-full bg-border overflow-hidden">
+      <div className="h-full w-1/4 bg-gold animate-loading-bar" />
+    </div>
+  )
+}
